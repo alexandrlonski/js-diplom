c0996476bb8abae5fb0d59d5b1fddb87f6ea46b0
Alexandr <aleksandr.lonski.86@gmail.com>
2020-11-26 01:22:07 +0300

    add all files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import 'nodelist-foreach-polyfill';
+import '@babel/polyfill';
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+
+import clubSelect from './modules/clubSelect';
+
+clubSelect();
---
+++ src/modules/clubSelect.js
@@ -0,0 +1,22 @@
+
+ const clubSelect = () => {
+  const listClubs = document.querySelector('.club-select ul'),
+        style = document.createElement('style');
+        style.setAttribute('type', 'text/css');
+        style.textContent = `
+          .active {
+            display: block !important;
+            z-index: 1005 !important;
+          }`; 
+        document.head.appendChild(style);
+  document.addEventListener('click', (e) => {
+    if(e.target.closest('.club-select p')){
+      listClubs.classList.toggle('active');  
+    } else if(!e.target.closest('.active') ){
+       listClubs.classList.remove('active');
+    }
+      
+  })
+};
+
+export default clubSelect;
