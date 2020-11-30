3984b02aaaf01105a21e7e377bbd2f70f3fe3203
Alexandr <aleksandr.lonski.86@gmail.com>
2020-11-30 03:11:53 +0300

    add responsive for slidercarusel

--- src/modules/sliderCarusel.js
+++ src/modules/sliderCarusel.js
@@ -4,21 +4,33 @@ const sliderCarusel = () => {
         sliderWrapper = document.querySelector('#services .wrapper'), 
         slides = servicesSlider.querySelectorAll('.slide'),
         btnPrev = sliderWrapper.querySelector('.prev'),
-        btnNext = sliderWrapper.querySelector('.next'),
-        slideToShow = 5,
-        widthSlide = Math.floor(100 / slideToShow);
-        let position = 0,
-            infinity = true;
-        // добавляем стили
-        sliderWrapper.style.overflow = 'hidden';
-        servicesSlider.style.justifyContent = 'space-between';
-        servicesSlider.style.padding = '0';
-        servicesSlider.style.transition = 'transform 0.5s';
-        servicesSlider.style.willChange = 'transform';
-        sliderWrapper.style.position = 'relative';
-        sliderWrapper.style.padding = '0 8px';
-        infinity = true;
+        btnNext = sliderWrapper.querySelector('.next');
         
+      let position = 0,
+          slideToShow = 5,
+          widthSlide = Math.floor(100 / slideToShow),
+          infinity = true;
+        // добавляем стили
+      sliderWrapper.style.overflow = 'hidden';
+      servicesSlider.style.justifyContent = 'space-between';
+      servicesSlider.style.padding = '0';
+      servicesSlider.style.transition = 'transform 0.5s';
+      servicesSlider.style.willChange = 'transform';
+      sliderWrapper.style.position = 'relative';
+      sliderWrapper.style.padding = '0 8px';
+      infinity = true;
+        
+        if(screen.width <= 1200 && screen.width > 991){
+          slideToShow = 4;
+          widthSlide = Math.floor(100 / slideToShow);
+        } else if(screen.width <= 991 && screen.width > 676 ) {
+          slideToShow = 3;
+          widthSlide = Math.floor(100 / slideToShow);
+        } else if(screen.width <= 676) {
+          slideToShow = 2;
+          widthSlide = Math.floor(100 / slideToShow);
+        }
+
         slides.forEach((item) => {
           item.style.flex = `0 0 ${widthSlide}%`;
           item.style.margin = '0';
